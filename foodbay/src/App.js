317379fde54c1e6f294b444d49270f7b5b5412ec
asv02 <200106025@hbtu.ac.in>
2023-08-27 13:00:35 +0530

    components

--- foodbay/src/App.js
+++ foodbay/src/App.js
@@ -1,10 +1,23 @@
 import Home from "./screens/Home";
+import Login from "./screens/Login";
+import {
+  BrowserRouter as Router,
+  Routes, Route
+} from "react-router-dom";
+
+//Router->div->Routes->Route
+
 function App() {
   return (
-    <div className="App">
-       <Home/>
-    </div>
-  );
+    <Router>
+      <div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/login" element={<Login/>}/>
+      </Routes>
+      </div>
+    </Router>
+    );
 }
 
 export default App;
--- foodbay/src/components/Navbar.jsx
+++ foodbay/src/components/Navbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </div>
-    </div>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container-fluid">
+          <Link className="navbar-brand fs-1 fst-italic" to={"/"}>FoodBay</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" aria-current="page" to={"./"}>Home</Link>
+              <Link className="nav-link" to={"./"}>Features</Link>
+              <Link className="nav-link" to={"./login"}>Login</Link>
+              {/* ./login path routing is set to login page using routes so no reloading happens*/}
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
