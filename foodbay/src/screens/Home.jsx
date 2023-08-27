370e53c8b10cc95c65b1c7978d5cdb23d0b8f2f7
asv02 <200106025@hbtu.ac.in>
2023-08-27 15:29:11 +0530

    Updated Home

--- foodbay/src/screens/Home.jsx
+++ foodbay/src/screens/Home.jsx
@@ -3,7 +3,23 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div>
-        <Navbar/>
+     <div> <Navbar /></div>
+      <div>
+        <div class="card mt-3" style={{"width":"18rem","maxHeight":"360px"}}>
+          <img src="." class="card-img-top" alt="..."/>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div className="container w-100">
+                <select className='m-2 h-100 w-100 bg-success'>
+                  {/* javascipt */}
+                  { }
+                </select>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
